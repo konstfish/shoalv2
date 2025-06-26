f154a77e31a51b9370db77c7dd96592f12017555
David Fischer <david@konst.fish>
2025-06-27 00:19:42 +0200

    chore(repo): add ci

---
+++ .github/renovate-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  repositories: ['konstfish/shoalv2'],
+  ignorePaths: ['archive/**'],
+  reviewers: ['konstfish'],
+  packageRules: [
+    {
+      matchUpdateTypes: ['minor', 'patch', 'pin', 'digest'],
+      automerge: true,
+      platformAutomerge: true
+    }
+  ],
+  platformAutomerge: true,
+  dependencyDashboard: false,
+  prCreation: 'immediate',
+  prHourlyLimit: 0,
+  automergeType: 'pr',
+  automergeStrategy: 'squash'
+};
